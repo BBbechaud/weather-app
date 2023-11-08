a6b31aa9bd0238d875bad0836ff96dc78c87ec6b
BBbechaud <132162306+BBbechaud@users.noreply.github.com>
2023-11-08 11:09:22 -0500

    First commit with some initial work done

---
+++ js/script.js
@@ -0,0 +1,21 @@
+let input = document.querySelector('input')
+let btn = document.querySelector('button')
+let temp = document.querySelector('.temp')
+
+btn.addEventListener('click', event => {
+    event.preventDefault()
+    getWeather(input.value) 
+    
+})
+
+async function getWeather(location) {
+   try{
+    const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=47f78b96ec8146cf91a151625230611&q=${location}&days=1&aqi=no&alerts=no`)
+    const weatherData = await response.json()
+    console.log(weatherData)
+    temp.innerHTML = (weatherData.forecast.forecastday[0].day.avgtemp_f)
+   }
+   catch(error) {
+        console.log("Please enter a correct city name") 
+    }
+}
